74f680653e07b509d5fd3235cb10bb1fba505819
Sayyed Ameen Naqvi <sayyedameennaqvi0430@gmail.com>
2025-11-04 19:14:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 
+
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from './Home';
+import About from './About';
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
